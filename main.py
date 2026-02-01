56b2471026ef791a588c35ab2aa7ad42384ea198
tokarboss <104645204+tokarboss@users.noreply.github.com>
2026-02-02 03:58:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import asyncio
+import asyncio1
 import aiosqlite
 import os
 import json
@@ -301,5 +301,6 @@ async def finish(message: types.Message, state: FSMContext):
 
 if __name__ == "__main__":
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+
 
